ca9200a72578652147356cd1d68adc480abaf9e2
programador1234 <hv12i04001@gmail.com>
2018-11-30 11:06:13 -0600

    agregado base fragment para multiples fragmentos en pila

--- app/src/main/java/com/example/rivas/salon_rmr/Activities/PrincipalActivity.java
+++ app/src/main/java/com/example/rivas/salon_rmr/Activities/PrincipalActivity.java
@@ -1,6 +1,9 @@
 package com.example.rivas.salon_rmr.Activities;
 
 
+
+
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -14,18 +17,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 
+import com.example.rivas.salon_rmr.Apputilities.BaseFragment;
 import com.example.rivas.salon_rmr.R;
 import com.example.rivas.salon_rmr.Fragment.HomeFragment;
 import com.example.rivas.salon_rmr.Fragment.ProductFragment;
 import com.example.rivas.salon_rmr.Fragment.ServiceFragment;
+import com.ncapdevi.fragnav.FragNavController;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrincipalActivity extends AppCompatActivity {
+import javax.annotation.Nullable;
+
+public class PrincipalActivity extends AppCompatActivity implements BaseFragment.FragmentNavigation,  FragNavController.TransactionListener, FragNavController.RootFragmentListener{
+
+
+    private final int INDEX_HOME    = FragNavController.TAB1;
+    private final int INDEX_PRODUCT = FragNavController.TAB2;
+    private final int INDEX_SERVICE = FragNavController.TAB3;
+    private final int INDEX_CONTACT = FragNavController.TAB4;
 
     ViewPager viewPager;
     AdaptadorFragmento adaptador;
+
+    private FragNavController mNavController;
+
     private Toolbar toolbar;
 
 
@@ -43,6 +61,16 @@ public class PrincipalActivity extends AppCompatActivity {
         setupViewPage(viewPager);
 
         toolbar = findViewById(R.id.toolbar);
+
+
+
+
+        mNavController = FragNavController.newBuilder(savedInstanceState, getSupportFragmentManager(), R.id.container)
+                .transactionListener(this)
+                .rootFragmentListener(this, 4)
+                .build();
+
+
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -77,6 +105,28 @@ public class PrincipalActivity extends AppCompatActivity {
         viewPager.setAdapter(adaptador);
 
     }
+
+    @NotNull
+    @Override
+    public Fragment getRootFragment(int i) {
+        return null;
+    }
+
+    @Override
+    public void onFragmentTransaction(@Nullable Fragment fragment, @NotNull FragNavController.TransactionType transactionType) {
+
+    }
+
+    @Override
+    public void onTabTransaction(@Nullable Fragment fragment, int i) {
+
+    }
+
+    @Override
+    public void pushFragment(Fragment fragment) {
+
+    }
+
     class AdaptadorFragmento extends FragmentPagerAdapter {
 
         private final List<Fragment> mFragmentList = new ArrayList<>();
