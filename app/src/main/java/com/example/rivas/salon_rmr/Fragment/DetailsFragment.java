5b28e8108c1215dbde89eacdb0c519ceedf9eb25
programador1234 <hv12i04001@gmail.com>
2018-12-06 16:44:02 -0600

    cambios

--- app/src/main/java/com/example/rivas/salon_rmr/Fragment/DetailsFragment.java
+++ app/src/main/java/com/example/rivas/salon_rmr/Fragment/DetailsFragment.java
@@ -1,9 +1,14 @@
 package com.example.rivas.salon_rmr.Fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +23,9 @@ import com.example.rivas.salon_rmr.Apputilities.DialogoContacto;
 import com.example.rivas.salon_rmr.Model.Item;
 import com.example.rivas.salon_rmr.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailsFragment extends BaseFragment {
 
     TextView txtDetalleTitulo,txtDetalleNombre,txtDetalleDescripcion,txtDetallePrecio;
@@ -26,6 +34,11 @@ public class DetailsFragment extends BaseFragment {
     private Item item;
     View view;
     CoordinatorLayout coordinatorLayout;
+
+    String num = "";
+    String msg = "";
+
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
 
     public void setItem(Item item) {
         this.item = item;
@@ -70,12 +83,50 @@ public class DetailsFragment extends BaseFragment {
     }
 
     private void contactarSalon() {
-        String num = PrincipalActivity.txtWhatsapp;
-        if(Contact.verificarContacto(getContext(),coordinatorLayout,num)){
-            DialogoContacto dialogo = new DialogoContacto();
-            dialogo.showDialog(getActivity(),num,"Me interesa el : "+txtDetalleNombre.getText().toString()+" que tiene un valor de "+txtDetallePrecio.getText().toString());
+        num = PrincipalActivity.txtWhatsapp;
+        msg =  "Me interesa el : " + txtDetalleNombre.getText().toString() + " que tiene un valor de " + txtDetallePrecio.getText().toString();
+        if(checkAndRequestPermissions()) {
+            if (Contact.verificarContacto(getContext(), coordinatorLayout, num)) {
+                DialogoContacto dialogo = new DialogoContacto();
+                dialogo.showDialog(getActivity(), num,msg);
+            }
         }
     }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_ID_MULTIPLE_PERMISSIONS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                if (Contact.verificarContacto(getContext(), coordinatorLayout, num)) {
+                    DialogoContacto dialogo = new DialogoContacto();
+                    dialogo.showDialog(getActivity(), num, msg);
+                }
+            } else {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Error al buscar en contactos", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        }
+    }
+
+    private  boolean checkAndRequestPermissions() {
+        int permissionREAD_CONTACT = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS);
+        int permissionWRITE_CONTACT = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_CONTACTS);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (permissionWRITE_CONTACT != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_CONTACTS);
+        }
+        if (permissionREAD_CONTACT != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_CONTACTS);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            requestPermissions(listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
 
 }
